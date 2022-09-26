1cc5b6aaeeec60f89836811dd6ac9573ec03b4d7
r.shodmon <r.s.shodmon@gmail.com>
2022-09-27 03:58:20 +0500

    New layout

---
+++ news/index.php
@@ -0,0 +1,5 @@
+<?php require_once (__DIR__.'/layout/head.php'); ?>
+<div class="w-8/12 mx-auto mt-4 shadow rounded-md p-4">
+    <?php require_once (__DIR__ . '/news.php'); ?>
+</div>
+<?php require_once (__DIR__.'/layout/footer.php'); ?>
